a51ecce6794991621cc2012aee4cba44ea61dceb
Chelynn Day <chelynnd@gmail.com>
2012-06-15 10:15:20 -0600

    merge commit

--- StudentContent/hasCompFinished.php
+++ StudentContent/hasCompFinished.php
@@ -7,13 +7,9 @@
 $compID = $_COOKIE['compID'];
 if(isset($_COOKIE['compID']) && $_COOKIE['compID'] != '')//If the competition has been created
 {
-	$con = mysql_connect("localhost", "guest", "");
-	mysql_select_db("competition", $con);
-	$sql = "SELECT * FROM usedids WHERE compID='${compID}'";
-	$result = mysql_query($sql);
-	$row = mysql_fetch_array($result);
+	$comp = new Competition($compID);
 	
-	if($row['hasfinished'] == 1)
+	if($comp->hasfinish)
 		echo 1;//Competition has ended
 	else
 		echo 0;//Competition has not ended
