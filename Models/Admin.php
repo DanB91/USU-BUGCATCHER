a2fc66df1a71d7665569d8e5b5d50140b6f53a43
Tara Noble <tara_noble@brown.edu>
2012-06-15 10:10:10 -0600

    changed connection

---
+++ Models/Admin.php
@@ -0,0 +1,61 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+require_once 'Model.php';
+/**
+ * Class the represents admin
+ *
+ * @author danielbokser
+ */
+class Admin extends Model{
+    
+    public function __construct($uniqueValue, $uniqueFieldName = FALSE) {
+        parent::__construct('ADMINS', $uniqueValue, $uniqueFieldName);
+    }
+    
+    public static function registerAdmin(array $registerData)
+    {
+        foreach($registerData as $fieldName => &$value)
+        {
+            if($fieldName === 'password')
+                continue;
+            
+            $value = "'" . $value . "'";
+        }
+        
+        Model::addRow('ADMINS', $registerData);
+    }
+    
+    /**
+     * If username and password match, this method returns an admin object, else it returns false
+     * @param type $username username of the admin
+     * @param type $password plain text password of the user
+     * @return boolean|\User the user itself if password matches the username (and username exists) , else false
+     */
+    public static function login($username, $password)
+    {
+        
+        try{
+            $admin = new Admin($username, 'username');
+            
+            if($admin->password !== crypt($password, SALT))
+                    return FALSE;
+            
+            return $admin;
+        }
+        catch(BugCatcherException $ex)
+        {
+            echo $ex->getMessage();
+            return FALSE;
+            
+        }
+        
+        
+        
+    }
+}
+
+?>
