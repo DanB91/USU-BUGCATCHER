5971b226022293695978ecf2be0b854643c6630a
Tara Noble <tara_noble@brown.edu>
2012-06-15 14:39:54 -0600

    working on testCaseText

--- StudentContent/testCaseText.php
+++ StudentContent/testCaseText.php
@@ -9,9 +9,9 @@ set_error_handler('error');
 
 
 /////error checks and defining variables///////
-$con = mysql_connect('localhost','guest','') or	trigger_error('Could not connect' . mysql_error());
+//$con = mysql_connect('localhost','guest','') or	trigger_error('Could not connect' . mysql_error());
 
-mysql_select_db("competition", $con);
+//mysql_select_db("competition", $con);
 
 
 $testInput  = mysql_real_escape_string(trim($_POST["testInput"])); //sanitizes input
@@ -20,116 +20,91 @@ $isCoverage = $_POST["codeCov"]; //boolean for coverage
 $problemName = $_POST["problemNum"];
 $compID 	= $_COOKIE['compID'];
 $userID		= $_COOKIE['userID'];
-$teamName       = $_SESSION['teamName'];
+$teamID       = $_SESSION['teamID'];
 
 
 if(!isset($_COOKIE['compID']) || $_COOKIE['compID'] == '' || $_COOKIE['userID'] == '' || !isset($_COOKIE['userID']))
 {
 	trigger_error('You must be part of the competion to submit bugs');
 }
-
+elseif(!isset($_SESSION['teamID']) || $teamID == '')
+{
+    trigger_error("You must be on a team to submit a bug");
+}
 //input and output cant be just white space or empty
 elseif (count(preg_split('/[\n\r\t\s]/', $testInput, NULL, PREG_SPLIT_NO_EMPTY)) == 0 || count(preg_split('/[\n\r\t\s]/', $testOutput, NULL, PREG_SPLIT_NO_EMPTY)) == 0)
 {
-	trigger_error('Please enter valid input');
+	trigger_error('Please enter an expected input and output');
 }
 elseif($problemName == '')
 {
 	trigger_error('Please select a problem'); 
 }
 
+
+$team = new Team($teamID);
+$prob = new Problem($problemName);
+$comp = new Competition($compID);
+$teamName = $team->teamname;
+
 $contentFileName = "C:/DropBox/htdocs/NewDesign/Competitions/$compID/$compID${teamName}Content.txt";//name of the content file
 
-//user must be on a team
-/*if(!$onTeam)
-{
-	trigger_error('User must be on a team');
-}*/
 ///////////////////////////////////
 
 chdir(ROOT_DIRECTORY);
 
-//$bugFileLoc = "Competitions/${compID}/${compID}${teamName}${problemName}Bugs.txt";
-$bugFilesInProblemDir = getBugFileNames($problemName);
-
-
 //now to run the oracle
-$oracle = trim(shell_exec("java -jar Problems/${problemName}/${problemName}Oracle.jar ${testInput}"));
+$oraclePath = $prob->oraclepath;
+$oracle = trim(shell_exec("java -jar ${oraclePath} ${testInput}"));
 
-//run the buggy code
-
-$buggyOutputs;//will hold the output of all the bugs 
-
-
-//var_dump($bugFilesInProblemDir);
-
-foreach($bugFilesInProblemDir as $index => $filePath)
-{
-	$buggyOutputs[$index] = trim(shell_exec("java -jar Problems/${problemName}/${filePath} ${testInput}"));
-}
-
-
-
-
+//get bugs from the file
+$bugs = $prob->bugs;
 
 $foundBug = '0'; //will test to see if the user found a bug 
 $alreadyFoundBug = false; //tests to see if the bug is already found
 $output = '';
-foreach($buggyOutputs as $index => $bO)
+foreach ($bugs as $index => $bug) 
 {
-	//if the supplied output is not equal to the oracle output, obviously the student did not find the bug
-	if($oracle != $testOutput)
-	{
-		break;
-	}
 
-	$bugName = "bug $index";
+    $filePath = $bug->filepath;
+    $buggyOutput = trim(shell_exec("java -jar ${filePath} ${testInput}"));
 
 
+    //bug possibly found!
+    if ($buggyOutput != $oracle) {
+        //test to see if the bug is already in the file
+        if ($team->hasFoundBugInCompetition($bug, $comp)) 
+        {
+            $alreadyFoundBug = true;
+            $output = $bO;
+            continue;
+        }
+        elseif ($oracle != $testOutput) 
+        {
+            break;
+        }
 
-	//bug possibly found!
-	if($bO != $oracle)
-	{
-		//test to see if the bug is already in the file
-		if(isLineInFile($bugName, $bugFileLoc))
-		{
-			$alreadyFoundBug = true;
-			$output = $bO;
-			continue;
-		}
-
-
-		
-
-		
-		//add the bug to the file
-		$bugFile = fopen($bugFileLoc, "a");
-		fwrite($bugFile, $bugName . PHP_EOL);
-		fclose($bugFile);
-
-		
-
-		//increment the number of bugs
-		mySQLUpdate("UPDATE ${compID}teams SET bugsFound= IFNULL(bugsFound, 0) + 1 WHERE teamName = '${teamName}'");
+        //increment the number of bugs
+        mySQLUpdate("UPDATE ${compID}teams SET bugsFound= IFNULL(bugsFound, 0) + 1 WHERE teamName = '${teamName}'");
 
 
-		//update leaderboard
-		$timeFile = file("Competitions/$compID/${compID}MasterTimer.txt");
-		$currentTime = intval($timeFile[0]);
-		
-		mySQLUpdate("UPDATE ${compID}teams SET timelastfound = ${currentTime} WHERE teamname = '${teamName}'");
-		mySQLUpdate("UPDATE ${compID}teams SET totaltimefound= IFNULL(totaltimefound, 0) + ${currentTime} WHERE teamname = '${teamName}'");
+        //update leaderboard
+        $timeFile = file("Competitions/$compID/${compID}MasterTimer.txt");
+        $currentTime = intval($timeFile[0]);
+
+        mySQLUpdate("UPDATE ${compID}teams SET timelastfound = ${currentTime} WHERE teamname = '${teamName}'");
+        mySQLUpdate("UPDATE ${compID}teams SET totaltimefound= IFNULL(totaltimefound, 0) + ${currentTime} WHERE teamname = '${teamName}'");
 
 
 
-		//alert the user they found a bug
-		writeToContentFile("[Test, $userName, $problemName]<br>Input: '$testInput'<br>Expected output: '$testOutput'<br>Actual Output: '$bO'<br>Bug found!", $contentFileName);
+        //alert the user they found a bug
+        writeToContentFile("[Test, $userName, $problemName]<br>Input: '$testInput'<br>Expected output: '$testOutput'<br>Actual Output: '$bO'<br>Bug found!", $contentFileName);
 
 
-		$foundBug = '1';
+        $foundBug = '1';
 
-		break;
-	}
+        break;
+    }
 }
 //if coverage is on, get the line numbers of the program that are executed
 chdir("Competitions/$compID/");
