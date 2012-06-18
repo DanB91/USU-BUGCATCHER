cf3803924653baa42ffdcd636c1aedb960f54aec
REU <reu@reu-OptiPlex-990.(none)>
2012-06-18 11:08:24 -0600

    Nothing

--- AdminCompContent/setMasterTimer.php
+++ AdminCompContent/setMasterTimer.php
@@ -1,13 +1,21 @@
 <?php
 session_start();
-$currComp = $_SESSION['currComp'];
-
-
 //Variable to create the Master Timer file and to write the starting competition time to it
 $time = $_GET['time'];
 $comp = $_COOKIE['adminCompID'];
+	
+//Opens the Master Timer file and writes the starting competition time to the file
+$timerFile = fopen("../Competitions/${comp}/${comp}MasterTimer.txt","w");
+if ($time < 10)
+{
+  fwrite($timerFile,"0".$time."00");
+}
+else
+{
+  fwrite($timerFile,$time."00");
+}
 
-$currComp->starttime = $_SERVER['REQUEST_TIME'];
-
+//Closes the Master Timer file and returns the time the Timer was set to for the competition back to the administrator
+fclose($timerFile);
 echo "Timer set to: ".$time." minutes.";
 ?>
