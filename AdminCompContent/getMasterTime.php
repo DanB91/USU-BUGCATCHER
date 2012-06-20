eede19e69b4a9d8f17dd0f4be35e3db16119fad0
reu <reu@reu-OptiPlex-990>
2012-06-20 16:30:50 -0600

    Separated the timestamp subtraction code

--- AdminCompContent/getMasterTime.php
+++ AdminCompContent/getMasterTime.php
@@ -2,139 +2,78 @@
 <?php
 
 require_once '../header.php';
+include 'timeStampManip.php';
 session_start();
-//Variable to create the Master Timer file and to write the starting competition time to it
+
 $admin = $_SESSION['adminObject'];
 $compN = $_COOKIE['compN'];
 
 
-    $duration = $admin->getCompetitionByCompName($compN)->duration;
     
+    $duration = $admin->getCompetitionByCompName($compN)->duration;
     $startTime = $admin->getCompetitionByCompName($compN)->starttime;
+  
     $startTime = substr($startTime, -8);
-    $startTimeArr = explode(":", $startTime);
     
     $currentTime = date("H:i:s");
     $currentTime = substr($currentTime, -8);
-    $currentTimeArr = explode(":", $currentTime);
 
-//Variables
-    $elapsedHours = '';
-    $elapsedMins = '';
-    $elapsedSecs = '';
-    $hoursLeft = '';
-    $minsLeft = '';
-    $secsLeft = '';
-
-
-//Precondition: None unless elapsedHours > 1 then currentTime mins must be greater than startTime mins
-//Postcondition: Sets the elapsedMins and elapsedSecs the competition has been running
-function getMinsSecs()
+if($admin->getCompetitionByCompName($compN)->hasfinish == 0)
 {
-    global $currentTimeArr, $startTimeArr, $elapsedMins, $elapsedSecs;
-    
-    $elapsedMins = $currentTimeArr[1] - $startTimeArr[1];
-      
-    
-    if($currentTimeArr[2] > $startTimeArr[2])//Seconds
-    {
-        $elapsedSecs = $currentTimeArr[2] - $startTimeArr[2];
-    }
-    else
-    {
-        $elapsedMins = $elapsedMins - 1;
-        $elapsedSecs = 60 - $startTimeArr[2];
-        $elapsedSecs = $elapsedSecs + $currentTimeArr[2];
-
-        if($elapsedSecs >= 60)
-        {
-            $elapsedSecs = $elapsedSecs - 60;
-            $elapsedMins = $elapsedMins + 1;
-        }
-    }
-    
-    
-}
-
-if($admin->getCompetitionByCompName($compN)->hasfinish === 1)
-{
-
-if($admin->getCompetitionByCompName($compN)->starttime !== '0000-00-00 00:00:00')
-{
-    
-    //var_dump($startTimeArr);
-    
-    if($currentTimeArr[0] == $startTimeArr[0])//If the hours are equal
-    {
-        getMinsSecs();
-    }
-    else
+    if($admin->getCompetitionByCompName($compN)->starttime !== '0000-00-00 00:00:00')
     {
         
-        if($currentTimeArr[1] > $startTimeArr[1])
+        if($admin->getCompetitionByCompName($compN)->pausestate == '0000-00-00 00:00:00')
         {
-            $elapsedHours = $currentTimeArr[0] - $startTimeArr[0];
-           
-            getMinsSecs();
-          
-            $elapsedMins = $elapsedMins + $elapsedHours*60;
+        
+                $diff = diffTime($currentTime, $startTime);
+                //echo $diff;
+                $diffArr = explode(":", $diff);
+                
+                $elapsedMins = $diffArr[0];
+                $elapsedSecs = $diffArr[1];
+                $secsLeft = '';
+                $minsLeft = '';
+                
+                if($elapsedSecs == 0)
+                {
+                    $minsLeft = $duration - $elapsedMins;
+                    $secsLeft = '00';
+
+                    if($secsLeft < 10)
+                        $secsLeft = '0' . $secsLeft;
+
+                    if($minsLeft < 10)
+                        $minsLeft = '0' . $minsLeft;
+
+                }
+                else 
+                {
+                    $minsLeft = $duration - $elapsedMins - 1;
+                    $secsLeft = 60 - $elapsedSecs;
+
+                    if($secsLeft < 10)
+                        $secsLeft = '0' . $secsLeft;
+
+                    if($minsLeft < 10)
+                        $minsLeft = '0' . $minsLeft;
+                }
+
+                $timeLeft = $minsLeft . $secsLeft;
+                echo  $timeLeft;
+                  
         }
         else
-        {
-            $elapsedHours = $currentTimeArr[0] - $startTimeArr[0] - 1;
-           
-            $tempSecs = 60 - $startTimeArr[2];
-            $tempMins = 60 - $startTimeArr[1];
-
-            $elapsedMins = $tempMins + $currentTimeArr[1];
-            $elapsedSecs = $tempSecs + $currentTimeArr[2];
-
-            if($elapsedSecs > 60)
-            {
-                $elapsedMins = $elapsedMins + 1;
-                $elapsedSecs = $elapsedSecs - 60;
-            }
-          
-
-            $elapsedMins = $elapsedMins + $elapsedHours*60;
-        }
-    }
-    
-       
-        if($elapsedSecs == 0)
-        {
-            $minsLeft = $duration - $elapsedMins;
-            $secsLeft = '00';
-
-            if($secsLeft < 10)
-                $secsLeft = '0' . $secsLeft;
-
-            if($minsLeft < 10)
-                $minsLeft = '0' . $minsLeft;
-
-        }
-        else 
-        {
-            $minsLeft = $duration - $elapsedMins - 1;
-            $secsLeft = 60 - $elapsedSecs;
-
-            if($secsLeft < 10)
-                $secsLeft = '0' . $secsLeft;
-
-            if($minsLeft < 10)
-                $minsLeft = '0' . $minsLeft;
-        }
-
-        $timeLeft = $minsLeft . $secsLeft;
-
-        echo  $timeLeft;
-   
+            echo "paused";
+ 
+ }
+    else
+        echo "0000";
 }
 else
-    echo "0000";
-}
-else
-    echo "STOP!";
+    echo "stop";
+
+
 ?>
 
 
