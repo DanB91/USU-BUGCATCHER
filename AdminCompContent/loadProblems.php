21e37d2ec2784e66361d6ec225ae33906ec22966
Mayo <Mayo@129.123.7.8>
2012-06-20 16:12:57 -0600

    add back in admin.js

--- AdminCompContent/loadProblems.php
+++ AdminCompContent/loadProblems.php
@@ -1,14 +1,21 @@
 <?php
+    session_start();
+    require '../Models/Problem.php';
+    require '../Models/Competition.php';
 
-    setcookie("adminUserName", "dan", time() + 60 * 60 * 60);
-     
-    if(isset($_COOKIE['adminUserName']) && $_COOKIE['adminUserName'] != '')//Admin is logged in
-    {
-        $fContents = scandir("../../USU-BUGCATCHER/Problems", 0);
-
-
-        $ret = json_encode($fContents);
-        echo $ret;
+        //$fContents = Competition::getProblems();
+    $arrayProblems = Problem::getProblems();
+    $currnetComps = $_SESSION['competitionObject']->getProblems();
+    $arrayOfProblems = array();
+    $fContents =array();
+    foreach($currnetComps as $value){array_push($arrayOfProblems,$value->problemname);}
+    foreach($arrayProblems as $value1){
+        if(!in_array($value1->problemname,$arrayOfProblems)){
+            array_push($fContents,$value1->problemname);
+        }   
     }
+    echo json_encode($fContents);
+     
+    
 
 ?>
--- Scripts/adminLandingPage.js
+++ Scripts/adminLandingPage.js
@@ -31,74 +31,24 @@ function CS_loadAdminComps()
 
 //Pre-Conditions:
 //Post-Conditions:
-function CS_joinComp()
-{
-    //
-    //
-    //
+function CS_joinComp(){
     if(compSelected == ""){
         alert("Please select a competition");
     }
     else{
-        if (window.XMLHttpRequest)
-        {// code for IE7+, Firefox, Chrome, Opera, Safari
-            CSSelectloadCompshttp=new XMLHttpRequest();
-        }
-        else
-        {// code for IE6, IE5
-            CSSelectloadCompshttp=new ActiveXObject("Microsoft.XMLHTTP");
-        }
-
-        CSSelectloadCompshttp.onreadystatechange=function()
-        {
-            if (CSSelectloadCompshttp.readyState == 4 && CSSelectloadCompshttp.status == 200)
-            {
-
+        if (window.XMLHttpRequest){CSSelectloadCompshttp=new XMLHttpRequest();}
+        else{CSSelectloadCompshttp=new ActiveXObject("Microsoft.XMLHTTP");}
+        CSSelectloadCompshttp.onreadystatechange=function(){
+            if (CSSelectloadCompshttp.readyState == 4 && CSSelectloadCompshttp.status == 200){
                 document.getElementById('CScontrol').innerHTML = CSSelectloadCompshttp.responseText;
                 compSelected = "";
             }
         }
-
         CSSelectloadCompshttp.open("GET","adminLoadAvailCompSelect.php?compID="+compSelected,true);
         CSSelectloadCompshttp.send();  
         alert("You now control this compeitiion");
         
     }
-    
-  /*
-  if (window.XMLHttpRequest)
-  {// code for IE7+, Firefox, Chrome, Opera, Safari
-    xmljoinComphttp=new XMLHttpRequest();
-  }
-  else
-  {// code for IE6, IE5
-    xmljoinComphttp=new ActiveXObject("Microsoft.XMLHTTP");
-  }
-  
-  xmljoinComphttp.onreadystatechange=function()
-  {
-    if (xmljoinComphttp.readyState == 4 && xmljoinComphttp.status == 200)
-    {
-        var t = xmljoinComphttp.responseText.trim();
-       // alert(t);
-        if(t == '1')
-        {
-            var studTName = prompt("This competition has started please enter your team name.");
-            while(studTName.search('"') >= 0 || studTName.search("'") >= 0)
-            {
-              studTName = prompt("Team name contained invalid characters. Please enter a different team name.");
-            }
-            if(studTName != null)
-                createSTeam(studTName);
-        }
-        else{
-            window.location = "Student.html";
-		}
-    }
-  }
-  xmljoinComphttp.open("GET","joinComp.php?compS="+compSelected,true);
-  xmljoinComphttp.send();
-    */
 }
 
 //Pre-Conditions:
@@ -171,64 +121,7 @@ function CS_getCompInfo()
   
   CSloadCompshttp.open("GET","adminLoadAvailCompsPreview.php?compID="+compSelected,true)
   CSloadCompshttp.send();
-  /*
   
-  
-  var loopCount = 1;
-  var displayOutput = "";
-            
-  if (window.XMLHttpRequest)
-  {// code for IE7+, Firefox, Chrome, Opera, Safari
-    getCompInfoXML=new XMLHttpRequest();
-  }
-  else
-  {// code for IE6, IE5
-    getCompInfoXML=new ActiveXObject("Microsoft.XMLHTTP");
-  }
-  
-  getCompInfoXML.open("GET","StudentContent/studentCompInfo.php?compID="+compSelected+"&fileLine="+3,true)
-  getCompInfoXML.send();
-  
-  getCompInfoXML.onreadystatechange=function()
-  {
-    if (getCompInfoXML.readyState == 4 && getCompInfoXML.status == 200)
-    {
-        switch(loopCount)
-        {
-          case 1:
-            var mins = getCompInfoXML.responseText;
-            displayOutput += "Competition ID: " + compSelected;
-            displayOutput += "\n\nCompetition Length: " + mins + " mins\n";
-            loopCount++;
-            getCompInfoXML.open("GET","StudentContent/AdminCompInfo.php?compID="+compSelected+"&fileLine="+0,true)
-            getCompInfoXML.send();
-            break;
-          case 2:
-            var probs = getCompInfoXML.responseText;
-            displayOutput += "\nNumber of Problems: " + probs + "\n";
-            loopCount++;
-            getCompInfoXML.open("GET","StudentContent/AdminCompInfo.php?compID="+compSelected+"&fileLine="+2,true)
-            getCompInfoXML.send();
-            break;
-          case 3:
-            var coverage = getCompInfoXML.responseText;
-            if (coverage == 0)
-            {
-              coverage="No";
-            }
-            else
-            {
-              coverage="Yes";
-            }
-            displayOutput += "\nCode Coverage Allowed: " + coverage;
-            document.getElementById('displayAdminCompInfo').value = displayOutput;
-            break;
-          default:
-            break;
-        }
-    }
-  }
-  */
 }
 
 //Pre-Conditions:
