a127b3d44cc272fbeab50b780f60130bf2415275
reu <reu@reu-OptiPlex-990>
2012-06-21 12:13:53 -0600

    Implemented prog and stats with the new api

--- ProgressContent/progressImpl.php
+++ ProgressContent/progressImpl.php
@@ -5,10 +5,35 @@
             
             $lb = new Leaderboard(new Competition($compN, "compname"));
             $stats = $lb->getStats();
-            var_dump($stats);
-            
+         
+            $counter = 1;
             //The purpose of this code is to show the admin the progrss of all teams and their place in the competition.
             //This can be viewd on the admin side under progress
-	?>
-	
+	?>	
+			
+	<?php while($counter <= count($stats) ) : ?>
+			  
+                    <table class="PTable" rules="all">
+                    <tr class="PRow">
+                        <td class="PTeamName"><?php echo $stats[$counter - 1]['teamName']//Display team name?></td>
+                        <td Class="Pstats"><?php 
+
+                             
+                            $bugsF = $stats[$counter - 1]['bugsFound'];
+                            if($bugsF == NULL)
+                                    $bugsF = 0;
+
+                            echo $bugsF;//Display total bugs found ?></td>
+                                <td Class="Pstats"><?php  
+                            //Display the time of last bug found
+                            $currentTime = $stats[$counter - 1]['lastTimeBugFound'];
+                            $currentTime = substr($currentTime, -8);
+                            
+                            echo $currentTime;
+
+                                                                         ?></td>
+                            
+                        <td Class="Pstats"><?php /*Display the place of the team in the competition*/echo $counter; $counter++;?></td>
+                    </tr>
+		<?php  endwhile;?>
 	
