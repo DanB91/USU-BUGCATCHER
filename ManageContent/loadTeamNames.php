f8843fa1c59ba8993d770373061e151da745f1d4
reu <reu@reu-OptiPlex-990>
2012-06-21 14:10:01 -0600

    Small changes were made

--- ManageContent/loadTeamNames.php
+++ ManageContent/loadTeamNames.php
@@ -2,37 +2,28 @@
  
  //The purpose of this code is to display the team names that have already been created so the admin can modify the team.
  //See removeStudentFromTeam.php for more details.
+require_once '../header.php';
+include '../timer.php';
+session_start();
+
+$admin = $_SESSION['adminObject'];
+$compN = $_COOKIE['compN'];
  
  
-if(isset($_COOKIE['adminCompID']) && $_COOKIE['adminCompID'] != '')//If a competition has been created.
+if(isset($_COOKIE['compN']) && $_COOKIE['compN'] != '')//If a competition has been created.
 {
-
-	$adminCompID = strtolower($_COOKIE['adminCompID']);
-
-	$con = mysql_connect("localhost","guest","");
-	mysql_select_db("competition", $con);
+        $teams = $admin->getCompetitionByCompName($compN)->getTeams();
+       
+	$team_name_string = '<select name="MTeamSelect" id="MTeamSelect" size=10 onchange="loadStudentInfo(this)">';//Start select
 	
-	if($results = mysql_query("SELECT * FROM ${adminCompID}teams"))//Used in the event that the competition cannot be found but the cookies have been set
-	{
-		;//Success
-	}
-	else
-		die("Competition not found. Please create another.");
-		
-	$team_String = '<select name="MTeamSelect" id="MTeamSelect" size=10 onchange="loadStudentInfo(this)">';//Start select
-	$names = '';
-	while($row = mysql_fetch_array($results))
-	{
-	  if(strcmp($row['teamname'],"") != 0)//Removes a blank element
-	  {
-		$name = $row['teamname'];
-		$NextOption = '<option>'.$name.'</option>';//Add option containing a team name
-		$team_String .= $NextOption;
-	  }
-	}
-	$team_String .= '</select>';//End select
-	echo $team_String;
-	mysql_close($con);
+        for($i = 0; $i < count($teams); $i++)
+        {
+            $name = $teams[$i]->teamname;
+            $team_name_string .= '<option>'.$name.'</option>';//Add option containing a team name
+        }
+	$team_name_string .= '</select>';//End select
+	echo $team_name_string;
+	
 }
 else
 	echo "You must create a competition" . "<br />" . "before you can edit team information." . "<br /><br /><br /><br /><br /><br /><br /><br /><br />";
