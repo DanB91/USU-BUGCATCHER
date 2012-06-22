d2541ea574d2165aadce4829ad4c9e73347c03fb
Mayo <Mayo@129.123.7.8>
2012-06-22 16:26:55 -0600

    Competition Information

--- StudentContent/testCaseText.php
+++ StudentContent/testCaseText.php
@@ -32,122 +32,49 @@ elseif ($problemName == '') {trigger_error('Please select a problem');}
 //chdir('../..');
 $prob = new Problem($problemName,"problemname");
 
-//* Remove '../Problems/'
-if(!file_exists('../Problems/'.$prob->oraclepath)){trigger_error("Can't find Oracle, contact administrative");}
+//* Remove 
+$tempFix = '../Problems/';
+if(!file_exists($tempFix.$prob->oraclepath)){trigger_error("Can't find Oracle, contact administrative");}
 
-$foundBug= 0;
-foreach($prob->getAllBugs() as $value){
-    $value->abpath;
+$foundBug = false;
+$alreadyFoundBug = false;
+$bugid ="";
+
+
+
+$oOutput = trim(shell_exec("java -jar $tempFix"."$prob->oraclepath $testInput"));
+
+
+foreach($prob->getBugs() as $value){
+    $bOutput = trim(shell_exec("java -jar $tempFix". "$value->abpath $testInput"));
+    
+     if ($bOutput != $oOutput) {
+         echo "$value->bugid,$comp->compid,$team->teamid";
+         if ($team->hasFoundBugInCompetition($value, $comp)) {
+             $alreadyFoundBug = true;
+             continue;
+         }
+         else{
+             $foundBug = true;
+             $bugid = $value->bugid;
+             break;
+         }
+         
+     }
     
 }
-
-
-
-///////////////////////////////////
-//now to run the oracle
-
-
-//get bugs from the file
-$bugs = $prob->bugs;
-
-$foundBug = '0'; //will test to see if the user found a bug 
-$alreadyFoundBug = false; //tests to see if the bug is already found
-$output = $oracle;
-foreach ($bugs as $index => $bug) {
-
-    $filePath = $bug->filepath;
-    $buggyOutput = trim(shell_exec("java -jar ${filePath} ${testInput}"));
-
-
-    //bug possibly found!
-    if ($buggyOutput != $oracle) {
-        //test to see if the bug is already in the file
-        if ($team->hasFoundBugInCompetition($bug, $comp)) {
-            $alreadyFoundBug = true;
-            $output = $buggyOutput;
-            continue;
-        } elseif ($oracle != $testOutput) {
-            $output = $buggyOutput;
-            break;
-        }
-        //increment the number of bugs
-        $team->foundBugInCompetition($bug, $comp);
-
-        //alert the user they found a bug
-        writeToContentFile("[Test, ".$user->username.", $problemName]<br>Input: '$testInput'<br>Expected output: '$testOutput'<br>Actual Output: '$buggyOutput'<br>Bug found!", $contentFileName);
-        $foundBug = '1';
-        break;
-    }
+if($foundBug &&  $alreadyFoundBug){
+    echo "Found Newer Bug";
+}
+elseif($foundBug &&  !$alreadyFoundBug){
+    echo "Found Bug";
+}
+elseif(!$foundBug &&  $alreadyFoundBug){
+    echo "Found alreay exist";
+}
+else{
+     echo "This is impossible";   
 }
 
-if (($lineNums = getExecutedLines($prob, $testInput, $team->teamid))) {
-    file_put_contents($comp->compid.$team->teamid."${problemName}Coverage.txt", implode("\n", $lineNums));
-}
-
-if ($alreadyFoundBug && !$foundBug) {
-    writeToContentFile("[Test, ".$user->username.", $problemName]<br>Input: '$testInput'<br>Expected output: '$testOutput'<br>Actual Output: '$output'<br>Bug already found!", $contentFileName);
-} else if (!$foundBug) {
-    writeToContentFile("[Test, ".$user->username.", $problemName]<br>Input: '$testInput'<br>Expected output: '$testOutput'<br>Actual output: '$oracle'<br>Bug not found!", $contentFileName);
-}
-
-echo $foundBug;
-
-
-//////functions//////////
-
-//returns an array of the line numbers of the executed lines of a program
-function getExecutedLines(Problem $prob, $testInput, $teamID)
-{
-	$lineNums = FALSE;
-	$command = "java -cp ../Problems/emma.jar emmarun -sp ../Problems/$problemName/src -r html -Dreport.html.out.file=$teamID$problemName/index.html -jar ../../Problems/$problemName/$problemName.jar $testInput";
-
-
-	//run emma to generate the code coverage
-	if(!($output = shell_exec($command)))
-		trigger_error('Emma call failed ' . $output);
-
-	$greenLines = file_get_html($teamID.$problemName.'/_files/1.html')->find('tr[class=c]');
-
-	//go through each element in outputted file by emma
-	foreach($greenLines as $element)
-	{
-		while($element->first_child())
-		{
-			$element = $element->first_child();
-			//echo $element;
-		}
-
-		$lineNums[] = intval($element->innertext) - 1;
-
-	}
-
-		return $lineNums;
-}
-
-
-
-//writes the given string to the content file
-function writeToContentFile($stringToWrite, $contentFileName)
-{
-	$contentFile = fopen($contentFileName,"a");
-	//put in time stamp
-	date_default_timezone_set('America/Denver');
-	fwrite($contentFile, (((date('H')*60)+date('i'))*60+date('s'))."<!@!>");
-
-	fwrite($contentFile, $stringToWrite . '<!@!>');
-	fclose($contentFile);
-
-}
-
-
-//error handler
-function error($errNo, $errStr)
-{
-	echo 'Error: ' . $errStr;
-	die();
-}
-
-////////////////////////////
-*/
 
 ?>
