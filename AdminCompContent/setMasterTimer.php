68ca414ab109c88d8f1e424bd456243b0526f099
reu <reu@reu-OptiPlex-990>
2012-06-22 13:30:27 -0600

    No changes

--- AdminCompContent/setMasterTimer.php
+++ AdminCompContent/setMasterTimer.php
@@ -11,7 +11,8 @@ if($admin->getCompetitionByCompName($compN)->starttime === '0000-00-00 00:00:00'
     $temp = $admin->getCompetitionByCompName($compN);
     $temp->starttime = date("Y-m-d H:i:s");
     $temp->commitToDB();
-    
+
 }
+
 
 ?>
