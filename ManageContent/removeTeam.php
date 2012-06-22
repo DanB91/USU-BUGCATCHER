ca6fedf9bdaee7bcb41e032facafd97bdf531aaf
reu <reu@reu-OptiPlex-990>
2012-06-22 14:12:13 -0600

    Changed how a team is removed from a competition

--- ManageContent/removeTeam.php
+++ ManageContent/removeTeam.php
@@ -9,45 +9,18 @@
 $team=$_GET["team"];
 $nullValue = NULL;
 
-if(isset($_COOKIE['adminCompID']) && $_COOKIE['adminCompID'] != '')
+require_once '../header.php';
+session_start();
+
+$admin = $_SESSION['adminObject'];
+$compN = $_COOKIE['compN'];
+
+if(isset($_COOKIE['compN']) && $_COOKIE['compN'] != '')
 {
-
-	$adminCompID = strtolower($_COOKIE['adminCompID']);
-
-	$con = mysql_connect("localhost","guest","");
-	mysql_select_db("competition", $con);
-
-	$sql="SELECT * FROM ${adminCompID}teams WHERE teamname = '".$team."'";
-
-	$result = mysql_query($sql);
-
-	$count = 1;
+        
+	$team = new Team($team, "teamname");
+	$team->removeTeamFromCompetition($admin->getCompetitionByCompName($compN));
 	
-	
-	while($row = mysql_fetch_array($result)){//Remove all students from team first
-
-		$memberNameToRemove = 'Member'."${count}".'name';
-		$memberIDToRemove = 'Member'."${count}".'ID';
-		
-		if($row["${memberNameToRemove}"] != NULL)
-		{
-			$temp = "UPDATE ${adminCompID}teams SET ${memberIDToRemove}='${nullValue}' WHERE teamname='${team}'";
-			mysql_query($temp);
-			$temp = "UPDATE ${adminCompID}students SET onteam='0' WHERE username='${userN}'";
-			mysql_query($temp);
-			$temp = "UPDATE ${adminCompID}teams SET ${memberNameToRemove}='${nullValue}' WHERE teamname='${team}'";
-			mysql_query($temp);
-			$temp = "UPDATE ${adminCompID}students SET teamName='${nullValue}' WHERE username='${userN}'";
-			mysql_query($temp);
-		}
-		
-		$count++;
-		
-	}
-	
-	mysql_query("DELETE FROM ${adminCompID}teams WHERE teamname='${team}'");//Remove team
-	
-	mysql_close($con);
 }
 else
 	echo"Cookies not set from removeTeam.php";
