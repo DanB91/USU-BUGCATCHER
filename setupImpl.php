b893f73914697886163c6d90f0c16a8f3d32fcc8
reu <reu@reu-OptiPlex-990>
2012-06-22 13:27:40 -0600

    Removed some code that was no longer needed

--- setupImpl.php
+++ setupImpl.php
@@ -6,8 +6,6 @@ require_once "Models/Admin.php";
 
 session_start();
 
-$nProbs = $_GET["numProbs"];
-$pArr = $_GET["problems"];
 $problemArr = array();
 
 $compN = $_GET["compN"];
@@ -17,12 +15,8 @@ $hidden = $_GET["hidden"];
 //$joinable = $_GET["joinable"];
 $compTime = $_GET["CompTime"];
 $codeCov = $_GET["codeCov"];
-$inclCD = $_GET["inclCD"];
 $admin = $_SESSION['adminObject'];
 
-
-
-    
 $pieces = explode(",", $_GET['problems'][0]);
 foreach($pieces as $value){
          $problem = new Problem($value,"problemname");
@@ -36,8 +30,7 @@ foreach($pieces as $value){
                     "description" => $desc,
                     "hidden" => $hidden,
                     "duration" => $compTime,
-                    "codecoverage" => 0,
-                    "countdown" => $inclCD
+                    "codecoverage" => $codeCov
                    );
 
 
